a4dc7ae3dc41795162ef312354f490d1b5c02914
Descifle <descifle_itb@yahoo.com>
2020-05-18 14:54:06 -0400

    updated jump to top

--- js/offset.js
+++ js/offset.js
@@ -1,3 +1,16 @@
+const jumpTop = document.querySelector('.nav-top')
+
 const shiftWindow = function() { scrollBy(0, -130) };
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
+
+const scrollTop = () => {
+    let y = window.scrollY
+    if (y >= 800) {
+        jumpTop.className = "nav-top show"
+    } else {
+        jumpTop.className = "nav-top hide"
+    }
+}
+
+window.addEventListener('scroll', scrollTop)
